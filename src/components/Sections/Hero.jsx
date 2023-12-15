7f0f15a04fafd915485d9715c4721a687ffb65bf
Dasajton <david.sajitz.94@gmail.com>
2023-12-15 17:52:27 +0100

    components folder with first Component Structure

---
+++ src/components/Sections/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero__container">
+        <h1 className="hero__title">
+          Hello, I'm <span className="hero__title--highlight">John Doe</span>
+        </h1>
+        <h2 className="hero__subtitle">
+          I'm a <span className="hero__subtitle--highlight">Web Developer</span>
+        </h2>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
