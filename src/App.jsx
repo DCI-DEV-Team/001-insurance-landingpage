4ccde1f116f6d5745ebd880f3b09a61879d9d640
Nik <niklasmoog@gmail.com>
2023-12-18 15:05:17 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import CardSection from './components/Sections/CardSection'
+import InfoSection from "./components/Sections/InfoSection"
 
 function App() {
   return (
     <>
       <h1>Hello</h1>
       <CardSection />
+		<InfoSection />
     </>
   )
 }
